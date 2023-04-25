0a8994116e9ec3c155891ebf96090b5799d4e57b
yaminiprasannachowdary <prasannachowdary9697@gmail.com>
2023-04-25 10:46:57 +0530

    maven project

---
+++ src/main/java/com/prsannachowdary/App.java
@@ -0,0 +1,13 @@
+package com.prsannachowdary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
